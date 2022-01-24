5fd860534c3c7d7ce125f9bbc82f9a2eb569f1a7
Rushali <rpatnaik78@gmail.com>
2022-01-24 15:29:31 +0530

    done with html

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <div id="app">
     <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
@@ -11,7 +10,19 @@
           @keypress="fetchWeather"
         />
       </div>
-    
+
+       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
