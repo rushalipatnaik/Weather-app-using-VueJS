c561ab8123e8a8f6139a6fbdd669ac3f4dfb3d42
Rushali <rpatnaik78@gmail.com>
2022-01-24 15:25:57 +0530

    working with html

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
- 
+    <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
     
   </div>
 </template>
